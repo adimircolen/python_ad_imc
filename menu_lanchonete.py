88ae8fed472c37460c7a61162ff98063e95c3e91
Adimir Colen <adimircolen@gmail.com>
2022-08-24 19:39:51 -0300

    Menu lanchonete plus

---
+++ menu_lanchonete.py
@@ -0,0 +1,14 @@
+opcao = ''
+while(True):
+    print('Menu Lanchonete Plus')
+    print('#'*5)
+    print('Sucos naturais')
+    print('Suco de morango')
+    print('Suco de Manga')
+    print('#'*5)
+    print('Salgados')
+    print('Pastel (queijo/carne)')
+    print('Coxinha')
+    opcao = input("Digite a opcao")
+    if opcao == '':
+        break
