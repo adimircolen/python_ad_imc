c2d93a6f9493366ee787b7698b7f12146f1304d8
Adimir Colen <adimircolen@gmail.com>
2022-08-24 20:35:43 -0300

    Correcao do bug

--- menu_lanchonete.py
+++ menu_lanchonete.py
@@ -2,13 +2,13 @@ opcao = ''
 while(True):
     print('Menu Lanchonete Plus')
     print('#'*5)
-    print('Sucos naturais')
-    print('Suco de morango')
-    print('Suco de Manga')
+    print('Sucos naturais - R$5,00')
+    print('Suco de morango - R$5,00')
+    print('Suco de Manga - R$5,00')
     print('#'*5)
     print('Salgados')
-    print('Pastel (queijo/carne)')
-    print('Coxinha')
+    print('Pastel (queijo/carne) - R$5,00')
+    print('Coxinha - R$5,00')
     opcao = input("Digite a opcao")
     if opcao == '':
         break
